fcffa486dcf9e109a0719cc7255c224f057edbc7
frankbungey <frankbungey@gmail.com>
2024-03-29 08:37:23 +0300

    assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+const showData = document.querySelector(".showdata");
+const btnOne = document.getElementById("location");
+const mapDiv = document.querySelector(".map")
+showData.style.display = "none"
+
+btnOne.addEventListener('click',()=>{
+    alert("Working on More ..thank youu")
+    showData.Style.display = "block";
+})
